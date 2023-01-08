a8f99f3506bae30717e56fa75080ad09630f7a33
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2023-01-08 16:06:02 +0530

    .

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Ket@nsag00",
+  password: "123456789",
   database: "labyrinth"
   
 });
--- routes/insertTeam.js
+++ routes/insertTeam.js
@@ -1,22 +1,21 @@
-
 const express = require("express");
 const router = express.Router();
 const mysql = require("../database/connection.js").con;
 
+router.post("/", (req, res) => {
+  const q = "INSERT INTO current_status(Team_ID) VALUES (?)";
+  const values = [req.body.Team_ID];
+  mysql.query(q, [values], (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.send({
+        code: -2,
+        message: err.message,
+      });
+    } else {
+      return res.status(200).json("Registered Successfully");
+    }
+  });
+});
 
-router.post("/",(req,res)=>{
- const q="INSERT INTO current_status(Team_ID) VALUES ?"
- const values=[
-   req.body.Team_ID
-   
- ]
- mysql.query(q,[values],(err,data)=>{
-  if(err) console.log(err)
-  return res.status(200).json("Registered Successfully")
- })
-
- 
-})
-
-
-module.exports=router;
+module.exports = router;
