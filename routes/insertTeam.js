1b3c1a7a7dc9e24a56e6415611b94b2906dd1323
Ketan Agrawal <ketanzz2003@gmail.com>
2023-01-08 11:43:26 +0530

    .

--- routes/insertTeam.js
+++ routes/insertTeam.js
@@ -5,10 +5,9 @@ const mysql = require("../database/connection.js").con;
 
 
 router.post("/",(req,res)=>{
- const q="INSERT INTO current_status(Team_ID) VALUES ?"
+ const q="INSERT INTO current_status(Team_ID) VALUES (?)"
  const values=[
    req.body.Team_ID
-   
  ]
  mysql.query(q,[values],(err,data)=>{
   if(err) console.log(err)
