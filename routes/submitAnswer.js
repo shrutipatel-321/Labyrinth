188ec75ddc92f3efb4fe9e4780e6f4844f5fc72a
Ketan Agrawal <ketanzz2003@gmail.com>
2023-01-11 19:38:39 +0530

    .

--- routes/submitAnswer.js
+++ routes/submitAnswer.js
@@ -9,7 +9,7 @@ router.post("/",middlewareComp, (req, res) => {
 
     const teamid = req.body.Team_ID;
     qind = "";
-    qnind = `SELECT current_ques_no FROM current_status WHERE Team_ID = ${teamid}`;
+    qnind = `SELECT current_ques_no, question_order, wrong_attempts FROM current_status WHERE Team_ID = ${teamid}`;
     mysql.query(qnind, (errslno, dataslno) => {
         if (errslno){ 
             res.json({
@@ -18,14 +18,9 @@ router.post("/",middlewareComp, (req, res) => {
             })
         }
         else {
-            if(dataslno.length == 0){
-                return res.json({
-                    code:-10,
-                    message:"No Team with given Team ID"
-                })
-            }
-            else{
                 qind = dataslno[0].current_ques_no -1 ; 
+                qo = dataslno[0].question_order;
+                wrgattempt = dataslno[0].wrong_attempts;
                 // if(qind<9){
                     // Fetching current question id
                     q1 = `SELECT current_ques_id FROM current_status WHERE Team_ID = ${teamid}`
@@ -39,11 +34,10 @@ router.post("/",middlewareComp, (req, res) => {
                         } 
                         else {
                             qid = data[0].current_ques_id;
-                            console.log(data[0].current_ques_id);
     
                             // Checking corrrect QR
-                            q2 = "SELECT qr_string FROM labyrinth_questions WHERE question_id = ?"
-                            mysql.query(q2, [qid], (err2, data2) => {
+                            q2 = "SELECT * FROM labyrinth_questions WHERE question_id = ? AND qr_string = ?"
+                            mysql.query(q2, [qid,req.body.qr_string], (err2, data2) => {
                                 if (err2){
                                     res.json({
                                         code: -2,
@@ -52,16 +46,16 @@ router.post("/",middlewareComp, (req, res) => {
                                 } 
                                 else {
                                     //If right QR scanned
-                                    if (data2[0].qr_string === req.body.qr_string) {
-                                        q3 = `SELECT question_order FROM current_status WHERE Team_ID = ?`
-                                        mysql.query(q3, [teamid], (err3, data3) => {
-                                            if (err3){
-                                                res.json({
-                                                    code: -3,
-                                                    message: err3.message
-                                                })
-                                            }
-                                            qo = data3[0].question_order;
+                                    if (data2.length!=0) {
+                                        // q3 = `SELECT question_order FROM current_status WHERE Team_ID = ?`
+                                        // mysql.query(q3, [teamid], (err3, data3) => {
+                                            // if (err3){
+                                            //     res.json({
+                                            //         code: -3,
+                                            //         message: err3.message
+                                            //     })
+                                            // }
+                                            // qo = dataslno[0].question_order;
                                             nextqid = qo[qind +1];
                                             q4 = "UPDATE current_status SET current_ques_id = ?, current_ques_no = ? WHERE Team_ID = ?"
                                             mysql.query(q4, [nextqid, qind + 2, teamid], (err4, data4) => {
@@ -71,36 +65,37 @@ router.post("/",middlewareComp, (req, res) => {
                                                         message: err4.message
                                                     })
                                                 }
-                                                else{
+                                                else{ 
                                                     res.status(200).json({
                                                     Status_code : 200,
                                                     code: 1,
-                                                    message:"Update question id Successfully"
+                                                    message:"Updated question id Successfully"
                                                     })
+                                                    
                                                 } 
                                             })
                                                 
                                             
-                                        })
+                                        // })
                                     }
                                     //If wrong QR scanned
                                     else {
                                         // res.json("False");
     
                                         //Increasing wrong attempts
-                                        q5 = `SELECT wrong_attempts FROM current_status WHERE Team_ID = ?`
-                                        mysql.query(q5, [teamid], (err5, data5) => {
+                                        // q5 = `SELECT wrong_attempts FROM current_status WHERE Team_ID = ?`
+                                        // mysql.query(q5, [teamid], (err5, data5) => {
                                             
-                                            if (err5) {
-                                                console.log("err5",err5)
-                                                res.json({
-                                                    code: -5,
-                                                    message: err5.message,
-                                                });
-                                            }
-                                            else {
-                                                wrgattempt = data5[0].wrong_attempts
-                                                console.log(data5[0])
+                                            // if (err5) {
+                                            //     console.log("err5",err5)
+                                            //     res.json({
+                                            //         code: -5,
+                                            //         message: err5.message,
+                                            //     });
+                                            // }
+                                            // else {
+                                                // wrgattempt = dataslno[0].wrong_attempts
+                                                console.log(dataslno[0])
                                                 if (wrgattempt > 10) {
                                                     res.json({
                                                         code: -8,
@@ -127,8 +122,8 @@ router.post("/",middlewareComp, (req, res) => {
                                                     })
                                                 }
                                             
-                                            }
-                                        })
+                                            // }
+                                        // })
                                     }
                                 }
                             })
@@ -142,7 +137,7 @@ router.post("/",middlewareComp, (req, res) => {
                 //         message:"You have finished the HUNT"
                 //     })
                 // }
-            }
+            
         }
     })
     
