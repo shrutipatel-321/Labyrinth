bda434288a9bf48b17b0c5141228316cc29c37e1
shaunak-09 <shaunakghosh09@kgpian.iitkgp.ac.in>
2023-01-12 21:48:54 +0530

    .

--- routes/insertTeam.js
+++ routes/insertTeam.js
@@ -16,44 +16,6 @@ router.post("/", (req, res) => {
   var easy_qs = [];
   var hard_qs = [];
 
-  
-    const q =
-      "SELECT question_id FROM labyrinth.labyrinth_questions WHERE difficulty='easy'";
-    mysql.query(q, (err, data) => {
-      // console.log(data);
-      if(err) console.log(err);
-      else{
-        for (let i = 0; i < data.length; i++) {
-          easy_qs.push(`${data[i].question_id}`);
-        }
-
-      }
-      
-      // console.log(easy_qs);
-      
-    });
-   
-  
- 
-    const q1 =
-      "SELECT question_id FROM labyrinth.labyrinth_questions WHERE difficulty='hard'";
-     
-      mysql.query(q1, (err, data) => {
-      // console.log(data);
-      if(err) console.log(err);
-      else{
-        for (let i = 0; i < data.length; i++) {
-          hard_qs.push(`${data[i].question_id}`);
-        }
-      }
-      
-      // console.log(hard_qs);
-    
-    });
-    
-
-    
-  
   function shuffle(array) {
     let currentIndex = array.length,
       randomIndex;
@@ -74,97 +36,120 @@ router.post("/", (req, res) => {
     return array;
   }
 
-   
-  setTimeout(()=>{
-    shuffle(easy_qs);
+  async function datafetching() {
+    const q =
+      "SELECT question_id FROM labyrinth.labyrinth_questions WHERE difficulty='easy'";
+    mysql.query(q, (err, data) => {
+      // console.log(data);
+      if (err) console.log(err);
+      else {
+        for (let i = 0; i < data.length; i++) {
+          easy_qs.push(`${data[i].question_id}`);
+        }
 
-    shuffle(hard_qs);
-    
-    easy_qs.map((item) => {
-      return q_array.push(item);
-    });
-    hard_qs.map((item) => {
-      return q_array.push(item);
-    });
-    
-  q_array.push("10");
-  // console.log(q_array);
+        const q1 =
+          "SELECT question_id FROM labyrinth.labyrinth_questions WHERE difficulty='hard'";
 
-  axios
-    .post("https://mainapi.springfest.in/api/user/get_registered_events", {
-      token: req.body.token,
-    })
-    .then((response) => {
-      if (response.data.code == 0) {
-        events = JSON.parse(JSON.stringify(response.data.message.group));
+        mysql.query(q1, (err, data) => {
+          // console.log(data);
+          if (err) console.log(err);
+          else {
+            for (let i = 0; i < data.length; i++) {
+              hard_qs.push(`${data[i].question_id}`);
+            }
 
-        const event = events.filter((item, i) => {
-          return item.event_name == "Centrifuge";
-        });
+            shuffle(easy_qs);
 
-        Team_ID = event[0]?.group_id;
-        console.log(Team_ID);
-        allmembers = event[0]?.members;
-        allmembers?.map((item) => {
-          return (
-            team_member_names.push(item.member_name),
-            team_member_sfids.push(item.member_sfid)
-          );
-        });
+            shuffle(hard_qs);
 
-        const last_updated = new Date();
-
-        const q =
-          "INSERT INTO labyrinth.current_status(Team_ID, current_ques_no, current_ques_id, wrong_attempts, last_updated, question_order, team_member_names, team_member_sfids) VALUES (?)";
-        const values = [
-          Team_ID,
-          q_array[0],
-          q_array[0],
-          10,
-          last_updated,
-          JSON.stringify(q_array),
-          JSON.stringify(team_member_names),
-          JSON.stringify(team_member_sfids),
-        ];
-        mysql.query(q, [values], (err, data) => {
-          if (err) {
-            console.log(err);
-            return res.send({
-              data: {
-                code: -3,
-                message: err.message,
-              },
+            easy_qs.map((item) => {
+              return q_array.push(item);
             });
-          } else {
-            return res.status(200).send({
-              data: {
-                code: 0,
-                message: "Registration successfull",
-              },
+            hard_qs.map((item) => {
+              return q_array.push(item);
             });
+
+            q_array.push("10");
+            // console.log(q_array);
+
+            axios
+              .post(
+                "https://mainapi.springfest.in/api/user/get_registered_events",
+                {
+                  token: req.body.token,
+                }
+              )
+              .then((response) => {
+                if (response.data.code == 0) {
+                  events = JSON.parse(
+                    JSON.stringify(response.data.message.group)
+                  );
+
+                  const event = events.filter((item, i) => {
+                    return item.event_name == "Centrifuge";
+                  });
+
+                  Team_ID = event[0]?.group_id;
+                  console.log(Team_ID);
+                  allmembers = event[0]?.members;
+                  allmembers?.map((item) => {
+                    return (
+                      team_member_names.push(item.member_name),
+                      team_member_sfids.push(item.member_sfid)
+                    );
+                  });
+
+                  const last_updated = new Date();
+
+                  const q =
+                    "INSERT INTO labyrinth.current_status(Team_ID, current_ques_no, current_ques_id, wrong_attempts, last_updated, question_order, team_member_names, team_member_sfids) VALUES (?)";
+                  const values = [
+                    Team_ID,
+                    q_array[0],
+                    q_array[0],
+                    10,
+                    last_updated,
+                    JSON.stringify(q_array),
+                    JSON.stringify(team_member_names),
+                    JSON.stringify(team_member_sfids),
+                  ];
+                  mysql.query(q, [values], (err, data) => {
+                    if (err) {
+                      console.log(err);
+                      return res.send({
+                        data: {
+                          code: -3,
+                          message: err.message,
+                        },
+                      });
+                    } else {
+                      return res.status(200).send({
+                        data: {
+                          code: 0,
+                          message: "Registration successfull",
+                        },
+                      });
+                    }
+                  });
+                }
+                if (response.data.code == -2)
+                  res.send({
+                    data: {
+                      code: -2,
+                      message: "Token expired",
+                    },
+                  });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
           }
         });
       }
-      if(response.data.code==-2)
-       res.send({
-        data:{
-          code:-2,
-          message:"Token expired",
-        }
-       })
-    })
-    .catch((err) => {
-      console.log(err);
     });
+  }
 
-  },50)
-    
-  
-  
-
-  
-
-  
+  datafetching();
 });
 
 module.exports = router;
