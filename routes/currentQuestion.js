82b89bfdb3c2a9d1329a7c0e940991f375b6d7ee
Ketan Agrawal <ketanzz2003@gmail.com>
2023-01-12 23:28:19 +0530

    .

--- routes/currentQuestion.js
+++ routes/currentQuestion.js
@@ -4,8 +4,8 @@ const mysql = require("../database/connection.js").con;
 
 router.get("/", (req, res) => {
 
-  const q = "SELECT * FROM labyrinth_questions WHERE ques_id = (?)";
-  const values = [req.body.ques_id];
+  const q = "SELECT current_ques_id FROM current_status WHERE Team_ID= (?)";
+  const values = [req.body.Team_ID];
   mysql.query(q, [values], (err, data) => {
     if (err) {
       console.log(err);
@@ -13,11 +13,34 @@ router.get("/", (req, res) => {
         code: -1,
         message: err.message,
       });
-    } else {
+    }
+    else {
       if (data.length != 0) {
-        return res.status(200).json(data[0].question_string);
-      } else {
-        return res.send("Question doesn't exist");
+        q2 = "SELECT question_string FROM labyrinth_questions WHERE question_id = (?)";
+        const values2 = [data[0].current_ques_id];
+
+        mysql.query(q2, [values2], (err, data2) => {
+          if (err) {
+            console.log(err);
+            return res.send({
+              code: -1,
+              message: err.message,
+            });
+          } else {
+            // console.log(data2)
+
+            return res.json({
+
+              code: 0,
+              question : data2[0].question_string
+            }
+            );
+          }
+          // return res.status(200).json(data[0].question_string);
+        });
+      }
+      else {
+        return res.send("Team doesn't exist");
       }
     }
   });
